97674633e0e05e501caee9093982e59dc176bbc5
Yunus Akçay <153110603+yunusakcay@users.noreply.github.com>
2024-07-28 18:31:48 +0300

    modified

--- music.js
+++ music.js
@@ -1,28 +1,28 @@
-class Music{
-    constructor(title, singer,img, file, type, id){
+class Music {
+    constructor(title, singer, img, file, type, id) {
         this.title = title;
         this.singer = singer;
         this.img = img;
         this.file = file;
         this.type = type;
-        this.id =id;
+        this.id = id;
     }
 
 }
 
 let musicList = [
-    new Music("Dudu","Tarkan","1.jpg","1.mp3","pop","0"),
-    new Music("Kuzu Kuzu","Tarkan","2.jpg","2.mp3","pop","1"),
-    new Music("Bangır Bangır","Gülşen","3.jpg","3.mp3","pop","2"),
-    new Music("Dansöz","Serdar Ortaç","4.jpg","4.mp3","pop","3"),
-    new Music("Kafama Göre","Athena","5.jpg","5.mp3","rock","4"),
-    new Music("Islak Islak","Barış Akarsu","6.jpg","6.mp3","rock","5"),
-    new Music("Her Şeyi Yak","Duman","7.jpg","7.mp3","rock","6"),
-    new Music("Mayın Tarlası","Şebnem Ferah","8.jpg","8.mp3","rock","7"),
-    new Music("Bir Derdim Var","Mor Ve Ötesi","9.jpg","9.mp3","rock","8"),
-    new Music("Gasolina","Daddy Yankee","10.jpg","10.mp3","yabanci","9"),
-    new Music("On The Floor","Jennifer Lopez","11.jpg","11.mp3","yabanci","10"),
-    new Music("Danza Kuduro","Don Omar","12.jpg","12.mp3","yabanci","11"),
-    new Music("Hips Dont Lie","Shakira","13.jpg","13.mp3","yabanci","12")
+    new Music("Dudu", "Tarkan", "1.jpg", "1.mp3", "pop", "0"),
+    new Music("Kuzu Kuzu", "Tarkan", "2.jpg", "2.mp3", "pop", "1"),
+    new Music("Bangır Bangır", "Gülşen", "3.jpg", "3.mp3", "pop", "2"),
+    new Music("Dansöz", "Serdar Ortaç", "4.jpg", "4.mp3", "pop", "3"),
+    new Music("Kafama Göre", "Athena", "5.jpg", "5.mp3", "rock", "4"),
+    new Music("Islak Islak", "Barış Akarsu", "6.jpg", "6.mp3", "rock", "5"),
+    new Music("Her Şeyi Yak", "Duman", "7.jpg", "7.mp3", "rock", "6"),
+    new Music("Mayın Tarlası", "Şebnem Ferah", "8.jpg", "8.mp3", "rock", "7"),
+    new Music("Bir Derdim Var", "Mor Ve Ötesi", "9.jpg", "9.mp3", "rock", "8"),
+    new Music("Gasolina", "Daddy Yankee", "10.jpg", "10.mp3", "yabanci", "9"),
+    new Music("On The Floor", "Jennifer Lopez", "11.jpg", "11.mp3", "yabanci", "10"),
+    new Music("Danza Kuduro", "Don Omar", "12.jpg", "12.mp3", "yabanci", "11"),
+    new Music("Hips Dont Lie", "Shakira", "13.jpg", "13.mp3", "yabanci", "12")
 ]
 
--- musicplayer.js
+++ musicplayer.js
@@ -1,6 +1,6 @@
-class MusicPlayer{
-  constructor(musicList){
-    this.musicList = musicList;  
+class MusicPlayer {
+  constructor(musicList) {
+    this.musicList = musicList;
     this.index = 0;
   }
 
@@ -8,22 +8,22 @@ class MusicPlayer{
     return this.musicList[this.index]
   }
 
-  next(){
-    
-    if(this.index + 1 == this.musicList.length){
-        this.index = 0;          
-    }else{
+  next() {
+
+    if (this.index + 1 == this.musicList.length) {
+      this.index = 0;
+    } else {
       this.index++;
     }
   }
 
-  previous(){
-    if(this.index != 0){
+  previous() {
+    if (this.index != 0) {
       this.index--;
-    }else{
+    } else {
       this.index = this.musicList.length - 1;
     }
   }
-  
+
 }
 
